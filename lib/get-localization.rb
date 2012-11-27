9e4290d9d1e86810fa9acf8ddaf446339282e574
Schuyler Ullman <schuyler.ullman@gmail.com>
2012-11-26 16:27:37 -0800

    Initial commit, skeleton gem.

---
+++ lib/get-localization.rb
@@ -0,0 +1,7 @@
+require "get-localization/version"
+
+module Get
+  module Localization
+    # Your code goes here...
+  end
+end
---
+++ lib/get-localization/version.rb
@@ -0,0 +1,5 @@
+module Get
+  module Localization
+    VERSION = "0.0.1"
+  end
+end
